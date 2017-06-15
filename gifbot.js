aa5d7fd1239dd420ba3be4dc8c407e4a432e0f1d
gingeneer <lukas.gygi@gmail.com>
2017-06-15 11:50:13 +0200

    added a bunch of stuff

--- gifbot.js
+++ gifbot.js
@@ -7,16 +7,33 @@ var token = variables['token'];
 var bot = new TelegramBot(token, { polling: true });
 console.log("bot started");
 
+bot.onText(/\/nomnom/, function (msg, match) {sendGif("food", msg);});
+
+bot.onText(/\/food/, function (msg, match) {sendGif("food", msg);});
+
+bot.onText(/\/drink/, function (msg, match) {sendGif("drink", msg);});
+
+bot.onText(/\/drunk/, function (msg, match) {sendGif("drunk", msg);});
+
+bot.onText(/\/pivo/, function (msg, match) {sendGif("beer", msg);});
+
+bot.onText(/\/beer/, function (msg, match) {sendGif("beer", msg);});
+
+bot.onText(/\/coffee/, function (msg, match) {sendGif("coffee", msg);});
+
+bot.onText(/\/chill/, function (msg, match) {sendGif("chill", msg);});
+
 bot.onText(/\/gif/, function (msg, match) {
-    var url = "";
     if (msg.text === "/gif" || msg.text === "/gif@RandomGif_spamBot") {
-        console.log("random gif");
-        url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=r"
+        sendGif("", msg);
     } else {
         var searchTerm = msg.text.replace(/\/gif/g, "").replace(/@RandomGif_spamBot/g, "").trim().replace(/ /g, "+");
-        console.log(searchTerm);
-        url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=r&tag=" + searchTerm;
+        sendGif(searchTerm, msg);
     }
+});
+
+function sendGif(tag, msg) {
+    url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=r&tag=" + tag;
     request({ url: url, json: true }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             if (body.data.length != 0) {
@@ -33,4 +50,4 @@ bot.onText(/\/gif/, function (msg, match) {
 
         }
     });
-});
+}
